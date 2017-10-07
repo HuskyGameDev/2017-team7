572fa32ec5692d05c527c9fba5ddbc01f9d8b726
WyldStalyns <aborman7@gmail.com>
2017-10-07 17:45:36 -0400

    Various changes
    - added inputs
    - added two characters and screens
    - added background map image

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D playerRB;
+    public int acceleration = 0;
+    public string playerNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("LeftStickX-" + playerNumber);
+        float moveVertical = Input.GetAxis("LeftStickY-" + playerNumber);
+
+        Vector2 movement = new Vector2(moveHorizontal * acceleration, moveVertical * acceleration);
+
+        playerRB.AddForce(movement);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
