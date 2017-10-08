8e70249e98677ba4b498ceb4233fcaa2099c67ee
denu8thell <zebrahome10@gmail.com>
2017-10-08 00:32:53 -0400

    Added in turning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
     private Rigidbody2D playerRB;
-    public int acceleration = 0;
-    public string playerNumber;
+    public float acceleration = 0;
+	public float turningSpeed = 0.5f;
+	public float maxSpeed = 5.0f;
+	public string playerNumber;
 
     // Use this for initialization
     void Start()
@@ -20,10 +23,24 @@ public class Player : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("LeftStickX-" + playerNumber);
         float moveVertical = Input.GetAxis("RightTrigger-" + playerNumber);
 
-        Vector2 movement = new Vector2(moveHorizontal * acceleration, moveVertical * acceleration);
+        //Vector2 movement = new Vector2(moveHorizontal * acceleration, moveVertical * acceleration);
 
-        playerRB.AddForce(movement);
-    }
+        //playerRB.AddForce(movement);
+		//Add torque to turn
+		playerRB.AddTorque (moveHorizontal*turningSpeed);
+		Vector2 newVel = new Vector2();
+		Vector2 accel = new Vector2 ();
+		// We get the rotation, convert to radians, and also add 90 degrees (PI/2 radians) to get our direction angle.
+		float rotation = (float)(Math.PI/180.0) * playerRB.rotation + (float)(Math.PI/2.0);
+		//new vel should be in the direction of rotation
+		newVel.Set ((float)Math.Cos (rotation), (float)Math.Sin (rotation));
+
+		accel = newVel * acceleration * moveVertical;
+		//Clamp to max speed
+		newVel = Vector2.ClampMagnitude((newVel * playerRB.velocity.magnitude) + accel, maxSpeed);
+
+		playerRB.velocity = newVel;
+	}
 
     void FixedUpdate()
     {
