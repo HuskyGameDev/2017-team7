8ae91de68071d89720313260c5023eee288ab147
jlfosdic <jlfosdic@mtu.edu>
2017-11-05 15:46:07 -0500

    Updated Controller.c to include all elements

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,58 +3,163 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour {
+public class Controller {
 
     private float turn;                         // Turning rotation
     private float forward;                      // Forward movement
     private float backward;
-    private float A;
-    private float B;
-    private float X;
-    private float Y;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Thumbsticks
+    private float lsXaxis;                      // Left stick X axis
+    private float lsYaxis;                      // Left stick Y axis
+    private float lsClick;                      // Left stick click
+    private float rsXaxis;                      // Right stick X axis
+    private float rsYaxis;                      // Right stick Y axis
+    private float rsClick;                      // Right stick click
+
+    // Triggers and bumpers
+    private float LT;                           // Left trigger
+    private float RT;                           // Right trigger
+    private float LB;                           // Left bumper
+    private float RB;                           // Right bumper
+
+    // Buttons
+    private float A;                            // A button
+    private float B;                            // B button
+    private float X;                            // X button
+    private float Y;                            // Y button
+    private float back;                         // back button
+    private float start;                        // start button
+
+    // D-Pad
+    private float dPadXaxis;                    // D-Pad X axis
+    private float dPadYaxis;                    // D-Pad Y axis
+
 
     // Update all input values
     public void UpdateValues(float t, float f, float b)
     {
-        turn = -t;
-        forward = f;
-        backward = b;
+        lsXaxis = -t;
+        RT = f;
+        LT = b;
     }
 
     // Update all input values
-    public void UpdateValues(float t, float f, float back, float a, float b,
-                                float x, float y)
+    public void UpdateValues(float lsX, float lsY, float lsC, float rsX, float rsY, float rsC,
+                                float lt, float rt, float lb, float rb,
+                                float a, float b, float x, float y, float ba, float st,
+                                float dpX, float dpY)
     {
-        turn = -t;
-        forward = f;
-        backward = back;
+        lsXaxis = lsX;
+        lsYaxis = lsY;
+        lsClick = lsC;
+        rsXaxis = rsX;
+        rsYaxis = rsY;
+        rsClick = rsC;
+
+        LT = lt;
+        RT = rt;
+        LB = lb;
+        RB = rb;
+
         A = a;
         B = b;
         X = x;
         Y = y;
+        back = ba;
+        start = st;
+
+        dPadXaxis = dpX;
+        dPadYaxis = dpY;
     }
+
+
+    //--------------------------------------------------------------------------------------------------------
+    // Race gets
 
     // Return turning value
     public float GetTurn()
     {
-        return turn;
+        return GetLsXaxis();
     }
 
     // Return speed value
     public float GetSpeed()
     {
-        return forward - backward;
+        return GetRT() - GetLT();
     }
+
+
+    //--------------------------------------------------------------------------------------------------------
+    // Thumbstick gets
+
+    // Return left stick X axis value
+    public float GetLsXaxis()
+    {
+        return lsXaxis;
+    }
+
+    // Return left stick Y axis value
+    public float GetLsYaxis()
+    {
+        return lsYaxis;
+    }
+
+    // Return left stick click value
+    public float GetLsClick()
+    {
+        return lsClick;
+    }
+
+    // Return right stick X axis value
+    public float GetRsXaxis()
+    {
+        return rsXaxis;
+    }
+
+    // Return right stick Y axis value
+    public float GetRsYaxis()
+    {
+        return rsYaxis;
+    }
+
+    // Return right stick click value
+    public float GetRsClick()
+    {
+        return rsClick;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------------
+    // Trigger and bumper gets
+
+    // Return left trigger value
+    public float GetLT()
+    {
+        return LT;
+    }
+
+    // Return right trigger value
+    public float GetRT()
+    {
+        return RT;
+    }
+
+    // Return left bumper value
+    public float GetLB()
+    {
+        return LB;
+    }
+
+    // Return right bumper value
+    public float GetRB()
+    {
+        return RB;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------------
+    // Button gets
 
     // Return A button value
     public float GetA()
@@ -78,5 +183,33 @@ public class Controller : MonoBehaviour {
     public float GetY()
     {
         return Y;
+    }
+
+    // Return back button value
+    public float GetBack()
+    {
+        return back;
+    }
+
+    // Return start button value
+    public float GetStart()
+    {
+        return start;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------------
+    // D-Pad gets
+
+    // Return D-Pad X axis value
+    public float GetDPadXaxis()
+    {
+        return dPadXaxis;
+    }
+
+    // Return D-Pad Y axis value
+    public float GetDPadYaxis()
+    {
+        return dPadYaxis;
     }
 }
