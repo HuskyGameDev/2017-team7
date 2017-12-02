fef7949c4fe48c4e014a991382c6ae95dcddf675
tleichte <tleichte@mtu.edu>
2017-12-02 14:47:27 -0500

    Allowed main menu to be used by both controller and mouse. Game advances to main scene upon any option.

--- Assets/Scripts/Menu/InputSelector.cs
+++ Assets/Scripts/Menu/InputSelector.cs
@@ -1,32 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InputSelector : MonoBehaviour
 {
 
     public EventSystem eventSystem;
-    public GameObject selectedObject; 
+    public GameObject firstObject;
+    private GameObject selectedObject;
+
+    bool mouseControl;
 
     // Use this for initialization
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        //selectedObject = eventSystem.firstSelectedGameObject;
+        mouseControl = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(eventSystem.currentSelectedGameObject);
-        if (eventSystem.currentSelectedGameObject != selectedObject)
+        if (!mouseControl)
         {
-            if (eventSystem.currentSelectedGameObject == null)
-                eventSystem.SetSelectedGameObject(selectedObject);
-            else
-                selectedObject = eventSystem.currentSelectedGameObject;
+            Cursor.lockState = CursorLockMode.None;
+            if (eventSystem.currentSelectedGameObject != selectedObject)
+            {
+                if (eventSystem.currentSelectedGameObject == null)
+                    eventSystem.SetSelectedGameObject(selectedObject);
+                else
+                    selectedObject = eventSystem.currentSelectedGameObject;
+            }
+            if (Input.GetKeyDown("joystick 1 button 0"))
+            {
+                selectedObject.GetComponent<Button>().onClick.Invoke();
+            }
+            if (Input.GetAxisRaw("Mouse_X") != 0 || Input.GetAxisRaw("Mouse_Y") != 0)
+            {
+                Cursor.visible = true;
+                mouseControl = true;
+                eventSystem.SetSelectedGameObject(null);
+            }
         }
-        Cursor.lockState = CursorLockMode.Locked;
+        if (mouseControl)
+        {
+            
+            if (Input.GetAxis("LeftStickY-1") != 0)
+            {
+                mouseControl = false;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                eventSystem.SetSelectedGameObject(firstObject);
+            }
+        }
+        
     }
 
 }
