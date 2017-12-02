24c4be88b7903617300964817124d16d2200c154
tleichte <tleichte@mtu.edu>
2017-12-02 14:12:29 -0500

    Made main menu button-controllable only

--- Assets/Scripts/Menu/InputSelector.cs
+++ Assets/Scripts/Menu/InputSelector.cs
@@ -6,28 +6,27 @@ public class InputSelector : MonoBehaviour
 {
 
     public EventSystem eventSystem;
-    public GameObject selectedObject;
-
-    private bool buttonSelected;
+    public GameObject selectedObject; 
 
     // Use this for initialization
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
+        //selectedObject = eventSystem.firstSelectedGameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("LeftStickY-1") != 0 && buttonSelected == false)
+        Debug.Log(eventSystem.currentSelectedGameObject);
+        if (eventSystem.currentSelectedGameObject != selectedObject)
         {
-            eventSystem.SetSelectedGameObject(selectedObject);
-            buttonSelected = true;
+            if (eventSystem.currentSelectedGameObject == null)
+                eventSystem.SetSelectedGameObject(selectedObject);
+            else
+                selectedObject = eventSystem.currentSelectedGameObject;
         }
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void OnDisable()
-    {
-        buttonSelected = false;
-    }
 }
