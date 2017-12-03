ea209ed9695009dd60e307d0e163ee21200b6ebc
WyldStalyns <aborman7@gmail.com>
2017-12-02 20:01:07 -0500

    Minimap stuff

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    public RectTransform location;
+    private Vector2 iconPos;
+    public Player player;
+
+	// Use this for initialization
+	void Start () {
+        iconPos = new Vector2(player.playerRB.position.x + 795, player.playerRB.position.y - 411);
+        location.position.Set(0, 0, 0);
+        Debug.Log(iconPos.ToString());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        location.position.Set(0, 0, 0);
+        Debug.Log(location.position.ToString());
+    }
+}
