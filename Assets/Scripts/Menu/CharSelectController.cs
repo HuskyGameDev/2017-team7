ab20f1424a50453f342bc22752bf1e56e0dd0cf3
tleichte <tleichte@mtu.edu>
2017-12-06 22:10:34 -0500

    Added Character Select Screen

---
+++ Assets/Scripts/Menu/CharSelectController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharSelectController : MonoBehaviour {
+
+    public Sprite[] images;
+    //public GameObject[] panels;
+
+    private int[] SelectedChars = { -1, -1, -1, -1 };
+    private string[] CharNames = { "Beefcake", "Fat Stacks", "Sheepish", "Vainglory" };
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void selectCharacter(int playernum, int index) {
+        SelectedChars[playernum] = index;
+    }
+    public void deselectCharacter(int playernum)
+    {
+        SelectedChars[playernum] = -1;
+    }
+    
+    public Sprite getCharImage(int index)
+    {
+        return images[index];
+    }
+
+    public string getCharName(int index)
+    {
+        return CharNames[index];
+    }
+
+    public bool charSelected(int index)
+    {
+        for (int i = 0; i < SelectedChars.Length; i++)
+        {
+            if (SelectedChars[i] == index) return true;
+        }
+        return false;
+    }
+
+    public int nextChar(int index)
+    {
+        //int next = index;
+        //bool done = false;
+        //while (!done)
+        //{
+        //    next++;
+        //    if (next >= SelectedChars.Length) next = 0;
+        //    if (index == next)
+        //    {
+        //        done = true;
+        //        break;
+        //    }
+        //    done = !charSelected(next);
+        //}
+        //return next;
+        int next = index + 1;
+        if (next >= SelectedChars.Length) next = 0;
+        return next;
+    }
+    public int prevChar(int index)
+    {
+        int next = index - 1;
+        if (next < 0) next = SelectedChars.Length - 1;
+        return next;
+        //int next = index;
+        //bool done = false;
+        //while (!done)
+        //{
+        //    next--;
+        //    if (next < 0) next = SelectedChars.Length - 1;
+        //    if (index == next)
+        //    {
+        //        done = true;
+        //        break;
+        //    }
+        //    done = !charSelected(next);
+        //}
+        //return next;
+    }
+
+    public bool canStart()
+    {
+        int readyPlayers = 0;
+        for (int i = 0; i < SelectedChars.Length; i++)
+        {
+            if (SelectedChars[i] >= 0)
+            {
+                readyPlayers++;
+                if (readyPlayers > 1) return true;
+            }
+        }
+        return false;
+    }
+
+    public void startGame()
+    {
+        if (canStart())
+        {
+            Debug.Log("Start");
+            //do communication prep
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/CharSelection.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharSelection : MonoBehaviour {
+
+	public int playerNumber;
+    public Image image;
+    public Text charName;
+    public Text pressStart;
+    public Image darkenPanel;
+    public CharSelectController controller;
+
+    private enum STATES { NONE, SELECTING, SELECTED };
+
+    private STATES state = STATES.NONE;
+
+    private int character = -1;
+
+    private bool axisBuffer;
+
+    void Start()
+    {
+        image.sprite = null;
+    }
+    void Update () {
+        switch (state)
+        {
+            case STATES.NONE:
+                pressStart.text = "Press Start to Join!";
+                charName.gameObject.SetActive(false);
+                pressStart.gameObject.SetActive(true);
+                image.gameObject.SetActive(false);
+                darkenPanel.gameObject.SetActive(true);
+                if (Input.GetKeyDown("joystick " + playerNumber + " button 7"))
+                {
+                    state = STATES.SELECTING;
+                    character = controller.nextChar(-1);
+                    image.sprite = controller.getCharImage(character);
+                }
+                break;
+            case STATES.SELECTING:
+                charName.gameObject.SetActive(true);
+                charName.text = controller.getCharName(character);
+                pressStart.gameObject.SetActive(false);
+                image.gameObject.SetActive(true);
+                darkenPanel.gameObject.SetActive(controller.charSelected(character));
+                if (Input.GetAxis("LeftStickX-" + playerNumber) < 0 && axisBuffer)
+                {
+                    character = controller.prevChar(character);
+                    image.sprite = controller.getCharImage(character);
+                    darkenPanel.gameObject.SetActive(controller.charSelected(character));
+                    axisBuffer = false;
+                }
+                if (Input.GetAxis("LeftStickX-" + playerNumber) > 0 && axisBuffer)
+                {
+                    character = controller.nextChar(character);
+                    image.sprite = controller.getCharImage(character);
+                    darkenPanel.gameObject.SetActive(controller.charSelected(character));
+                    axisBuffer = false;
+                }
+                if (Input.GetKeyDown("joystick " + playerNumber + " button 0"))
+                {
+                    if (!controller.charSelected(character))
+                    {
+                        controller.selectCharacter(playerNumber - 1, character);
+                        image.sprite = controller.getCharImage(character);
+                        state = STATES.SELECTED;
+                    }
+                    
+                }
+                if (Input.GetKeyDown("joystick " + playerNumber + " button 1"))
+                {
+                    character = -1;
+                    state = STATES.NONE;
+                }
+                break;
+            case STATES.SELECTED:
+                pressStart.gameObject.SetActive(true);
+                darkenPanel.gameObject.SetActive(true);
+                pressStart.text = "READY";
+                if (Input.GetKeyDown("joystick " + playerNumber + " button 1")) //b
+                {
+                    controller.deselectCharacter(playerNumber - 1);
+                    state = STATES.SELECTING;
+                }
+                if (Input.GetKeyDown("joystick " + playerNumber + " button 7")) //start
+                {
+                    controller.startGame();
+                }
+                break;
+        }	
+        
+        if (Input.GetAxis("LeftStickX-" + playerNumber) == 0)
+        {
+            axisBuffer = true;
+        }	
+	}
+
+    public int getSelChar()
+    {
+        return character;
+    }
+}
