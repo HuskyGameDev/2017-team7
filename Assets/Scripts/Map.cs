545699bb48a74ee03316486b9dbdb73c25fee52c
WilliamCollicott <31823020+WilliamCollicott@users.noreply.github.com>
2018-01-28 11:30:12 -0500

    Added Pause Mechanic

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class Map : MonoBehaviour {
+
+	PlayerIndex pausePlayerIndex;
+    GamePadState pausePlayerState;
+    GamePadState pausePlayerPrevState;
+	bool paused = false;
+
+	GamePadState[] testStates = new GamePadState[4];
 
 	// Use this for initialization
 	void Start () {
@@ -11,10 +19,45 @@ public class Map : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (!paused) {
+			for (int i = 0; i < 4; i++) {
+
+				PlayerIndex testPlayerIndex = (PlayerIndex) i;
+				GamePadState testState = GamePad.GetState(testPlayerIndex);
+
+				if (testState.IsConnected) {
+					
+					if (Barnout.ButtonPressed(testState.Buttons.Start, 
+										testStates[i].Buttons.Start)) {
+						pausePlayerIndex = testPlayerIndex;
+						pausePlayerState = testState;
+						if (Time.timeScale == 1.0F) {
+							Time.timeScale = 0.0F;
+							paused = true;
+							Debug.Log("PAUSED");
+						}
+					}
+				}
+				testStates[i] = testState;
+			}
+		}
+		else {
+			pausePlayerPrevState = pausePlayerState;
+			pausePlayerState = GamePad.GetState(pausePlayerIndex);
+			if (Barnout.ButtonPressed(pausePlayerState.Buttons.Start, 
+										pausePlayerPrevState.Buttons.Start)) {
+				Time.timeScale = 1.0F;
+				paused = false;
+				Debug.Log("NOT PAUSED");
+			}
+								
+		}
+
 		
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
+    private void OnTriggerEnter2D(Collider2D collision) {
+
     }
 }
