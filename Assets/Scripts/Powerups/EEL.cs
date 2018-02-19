d8bd3df1a51db0c0b7dfb098a9758ca3dcf459c7
WilliamCollicott <31823020+WilliamCollicott@users.noreply.github.com>
2018-02-18 19:02:26 -0500

    Added Eel Powerup.
    Still need anomation/image that surrounds the kart.

--- Assets/Scripts/Powerups/EEL.cs
+++ Assets/Scripts/Powerups/EEL.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EEL : Powerup {
+public class Eel : Powerup {
+
+	public GameObject EelObject;
+
+	IEnumerator endEel(float time) {
+		yield return new WaitForSeconds(time);
+		EelObject.SetActive(false);
+	}
 
 	// Use this for initialization
 	void Start () {
 		currentCooldown = 0;
 	}
+
 	
 	public override bool UsePowerup() {
 		if(base.UsePowerup()) {
@@ -15,7 +23,10 @@ public class EEL : Powerup {
 			//Change player image to the one with the eels surrounding the kart here.
 			//.....
 
-			//Activate the EELtrigger script that incapacitates the player that collided with the player who has the eel powerup running. Is this done by instantiating an object?
+			EelObject.SetActive(true);
+
+			StartCoroutine(endEel(3f));
+			
 			
 			Debug.Log("PLAYER " + owner.playerNumber + " USED EEL POWERUP, " + uses + " USES ARE LEFT.");
 			return true;
