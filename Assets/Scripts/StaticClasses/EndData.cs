d02a50e4033d90d1f042b0f72776a28188242691
jlfosdic <jlfosdic@mtu.edu>
2018-02-18 20:00:09 -0500

    Smooth end transition for two players

--- Assets/Scripts/StaticClasses/EndData.cs
+++ Assets/Scripts/StaticClasses/EndData.cs
@@ -10,17 +10,50 @@ public static class EndData {
 
     public static void EndTransition()
     {
+        foreach (Player p in PlayerData.GetActivePlayers())
+        {
+            Animator animator = p.GetComponentInChildren<Animator>();
+            if (p.playerNumber == completionOrder[0])
+            {
+                animator.SetTrigger("Win");
+            }
+            else
+            {
+                animator.SetTrigger("Lose");
+            }
+        }
+
+        /*Player winner = PlayerData.GetPlayerByNumber(completionOrder[0]);
+        //Player loser = PlayerData.GetPlayerByNumber(1);
+
+        CameraControl cam = winner.GetComponentInChildren<CameraControl>();
+
+        cam.Win();
+
+        foreach ()
+        //Camera camLose = loser.GetComponentInChildren<Camera>();
+
+        /*cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 20, zoomSpeed);
+        Vector3 v = new Vector3(cam.gameObject.transform.localPosition.x, Mathf.Lerp(cam.gameObject.transform.localPosition.y, 0, zoomSpeed),
+            cam.gameObject.transform.localPosition.z);
+        cam.gameObject.transform.localPosition = v;
+
+        Rect r = new Rect(Mathf.Lerp(cam.rect.x, 0, zoomSpeed), Mathf.Lerp(cam.rect.y, 0, zoomSpeed),
+            Mathf.Lerp(cam.rect.width, 1, zoomSpeed), Mathf.Lerp(cam.rect.height, 1, zoomSpeed));
+        cam.rect = r;
+
+
+       // r = new Rect(Mathf.Lerp(camLose.rect.x, 0, zoomSpeed), Mathf.Lerp(camLose.rect.y, 0, zoomSpeed),
+       //     Mathf.Lerp(camLose.rect.width, 0, zoomSpeed), Mathf.Lerp(camLose.rect.height, 1, zoomSpeed));
+        //camLose.rect = r;*/
+    }
+
+    public static void EndZoom()
+    {
         Player winner = PlayerData.GetPlayerByNumber(completionOrder[0]);
 
         Camera cam = winner.GetComponentInChildren<Camera>();
 
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 20, zoomSpeed);
-        Vector3 v = new Vector3(cam.gameObject.transform.localPosition.x, Mathf.Lerp(cam.gameObject.transform.localPosition.y, 0, zoomSpeed),
-            cam.gameObject.transform.localPosition.z);
-        cam.gameObject.transform.localPosition = v;
-
-        /*Rect r = new Rect(Mathf.Lerp(cam.rect.x, 0, zoomSpeed), Mathf.Lerp(cam.rect.y, 0, zoomSpeed),
-            Mathf.Lerp(cam.rect.width, 1, zoomSpeed), Mathf.Lerp(cam.rect.height, 1, zoomSpeed));
-        cam.rect = r;*/
     }
 }
