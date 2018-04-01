6a25655f489631264be4c2909861bd1b2ca1215f
jlfosdic <jlfosdic@mtu.edu>
2018-04-01 17:54:25 -0400

    Testing for potential dodgeball like gametype/minigame

---
+++ Assets/JoshTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Test for a potential future dodgeball like gametype/minigame.
+// Players start 27 X difference, 22 Y difference
+
+public class JoshTest : Minigame {
+
+    public float timeGiven;
+    private bool done = false;
+    private Coroutine timer;
+    public int lives;
+    public Players playerCars;
+    private int playersOut = 0;
+
+    protected override void InitMinigame()
+    {
+        timer = StartCoroutine(BeginTimer());
+        foreach (JoshTestPlayer p in players)
+        {
+            p.Lives = lives;
+        }
+    }
+
+    public override void Tick()
+    {
+        foreach (Player p in playerCars.players)
+        {
+            if (p.state == Player.STATES.INCAPACITATED)
+            {
+                // ((JoshTestPlayer)players[p.playerNumber - 1]) is BAD!!!!! NOT RIGHT P{LAYR
+                ((JoshTestPlayer)players[p.playerNumber - 1]).Lives--;
+                if (((JoshTestPlayer)players[p.playerNumber - 1]).Lives <= 0)
+                {
+                    ((JoshTestPlayer)players[p.playerNumber - 1]).SetScore(++playersOut);
+                }
+            }
+        }
+
+        if (playersOut == PlayerData.instance.numPlayers)
+        {
+            done = true;
+        }
+    }
+
+    public override bool HasEnded()
+    {
+        return done;
+    }
+
+    IEnumerator BeginTimer()
+    {
+        yield return new WaitForSecondsRealtime(timeGiven);
+        Debug.Log("Timer done");
+        Done();
+    }
+
+    void Done()
+    {
+        Debug.Log("Finished");
+        done = true;
+    }
+}
---
+++ Assets/JoshTestPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Player for dodgeball game.
+
+public class JoshTestPlayer : MinigamePlayer {
+
+    public int Lives { get; set; }
+
+    protected override void Init()
+    {
+        score = 4;
+    }
+
+    protected override void Tick()
+    {
+        
+    }
+
+    protected override void OnGameDone()
+    {
+        
+    }
+
+    public void SetScore(int s)
+    {
+        score = s;
+    }
+}
