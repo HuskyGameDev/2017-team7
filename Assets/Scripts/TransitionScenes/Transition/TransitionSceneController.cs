7e6372add7d05346296570ed80ae2b63156d5772
tleichte <tleichte@mtu.edu>
2018-04-15 09:46:00 -0400

    Set up transition functionality

--- Assets/Scripts/TransitionScenes/Transition/TransitionSceneController.cs
+++ Assets/Scripts/TransitionScenes/Transition/TransitionSceneController.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 
 public class TransitionSceneController : MonoBehaviour {
 
-    private enum STATES { }
+    private enum STATES { CHOOSING_POWERUP, CHOOSING_SLOT, WAITING }
+    private STATES state = STATES.WAITING;
+    Animator animator;
 
+    private int currPlayer;
+    private int currPlace = 0;
+
+    Controller currController;
+    List<MinigameData.Standing> standings;
+    MinigameData.Standing currStanding;
 
     public void Start()
     {
+        standings = new List<MinigameData.Standing>(MinigameData.standings);
+        animator = GetComponent<Animator>();
+        
         //Get 4 random powerups
         System.Random random = new System.Random();
         List<PowerupType> values = new List<PowerupType>((PowerupType[])Enum.GetValues(typeof(PowerupType)));
@@ -17,7 +28,36 @@ public class TransitionSceneController : MonoBehaviour {
 
     public void FixedUpdate()
     {
-        
+        switch (state)
+        {
+            case STATES.CHOOSING_POWERUP:
+
+
+                break;
+            case STATES.CHOOSING_SLOT:
+
+
+                break;
+        }
     }
+
+
+    public void ToChoosing()
+    {
+        state = STATES.CHOOSING_POWERUP;
+    }
+
+    public void NextPlayer()
+    {
+        currPlace++;
+        foreach(MinigameData.Standing s in standings)
+        {
+            if (s.standing == currPlace)
+            {
+                currStanding = s;
+            }
+        }
+    }
+
 
 }
