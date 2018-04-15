62d798bdde6f1be2e52625c7fb563fb5575ef8df
tleichte <tleichte@mtu.edu>
2018-04-15 12:01:38 -0400

    Added more functionality to transitions

---
+++ Assets/Scripts/TransitionScenes/Transition/TransitionStickPowerup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionStickPowerup : MonoBehaviour {
+
+    [UnityEngine.SerializeField]
+    public SpriteRenderer coin;
+
+    public void SetTaken(Sprite newCoin)
+    {
+        coin.gameObject.SetActive(true);
+        coin.sprite = newCoin;
+    }
+
+    // Use this for initialization
+    void Start () {
+        coin.gameObject.SetActive(false);
+	}
+}
--- Assets/Scripts/TransitionScenes/Transition/TransitionSticks.cs
+++ Assets/Scripts/TransitionScenes/Transition/TransitionSticks.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class TransitionSticks : MonoBehaviour {
 
+    public TransitionStickPowerup powerup3x;
+    public TransitionStickPowerup powerup2x_l;
+    public TransitionStickPowerup powerup2x_r;
+    public TransitionStickPowerup powerup1x;
+
+
+    
+
+
+
+
 
 }
