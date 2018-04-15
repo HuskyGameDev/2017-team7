d0ab7bbea8c2b7b666adad3d5f1d4c213ffd00d2
tleichte <tleichte@mtu.edu>
2018-04-14 23:23:44 -0400

    Finished animations for transitions - ADD FUNCTIONALITY

---
+++ Assets/Scripts/TransitionScenes/Transition/TransitionSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionSceneController : MonoBehaviour {
+
+    private enum STATES { }
+
+
+    public void Start()
+    {
+        //Get 4 random powerups
+        System.Random random = new System.Random();
+        List<PowerupType> values = new List<PowerupType>((PowerupType[])Enum.GetValues(typeof(PowerupType)));
+    }
+
+    public void FixedUpdate()
+    {
+        
+    }
+
+}
