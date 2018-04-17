1310f8a118093442eedf6ce46047c2b0b9fa9fb4
tleichte <tleichte@mtu.edu>
2018-04-17 00:45:11 -0400

    Added end scene functionality

---
+++ Assets/Scripts/End/EndSceneController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndSceneController : MonoBehaviour {
+
+    public EndScenePlayer[] players;
+
+    private bool done;
+
+    public Sprite[] winnerSprites;
+    public Sprite[] loserCoins;
+    public Sprite[] playerCoins;
+
+	// Use this for initialization
+	void Start ()
+    {
+        done = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!done)
+        {
+            done = true;
+            foreach (EndScenePlayer p in players)
+            {
+                if (p.GetState() == EndScenePlayerState.SELECTING)
+                {
+                    done = false;
+                    break;
+                }
+            }
+
+
+            if (done)
+            {
+                bool rematch = true;
+                foreach (EndScenePlayer p in players)
+                {
+                    if (p.GetState() != EndScenePlayerState.REMATCH)
+                    {
+                        rematch = false;
+                        break;
+                    }
+                }
+                if (rematch)
+                {
+                    StartCoroutine(BackToMainMenu());
+                }
+                else
+                {
+                    StartCoroutine(StartRematch());
+                }
+            }
+        }
+        
+    }
+
+    IEnumerator BackToMainMenu()
+    {
+        yield return new WaitForSecondsRealtime(2.0f);
+        Barnout.ChangeScene("MainMenu");
+    }
+
+    IEnumerator StartRematch()
+    {
+        yield return new WaitForSecondsRealtime(2.0f);
+        List<BarnoutPlayer> barnoutPlayers = new List<BarnoutPlayer>();
+        foreach(BarnoutPlayer p in PlayerData.instance.barnoutPlayers)
+        {
+            barnoutPlayers.Add(new BarnoutPlayer(p.IsActive(), p.GetCharacter(), p.GetPlayerNum()));
+        }
+        PlayerData.Instantiate(barnoutPlayers.ToArray());
+        Barnout.ChangeScene("MinigameStart");
+        //TODO do rematch code
+    }
+
+
+
+    public Sprite GetWinnerSprite(int charNum)
+    {
+        return winnerSprites[charNum];
+    }
+
+    public Sprite GetPlayerCoin(int playerNum)
+    {
+        return playerCoins[playerNum - 1];
+    }
+
+    public Sprite GetLoserCoin(int charNum)
+    {
+        return loserCoins[charNum];
+    }
+
+}
---
+++ Assets/Scripts/End/EndScenePlayer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EndScenePlayerState
+{
+    INACTIVE,
+    SELECTING,
+    CONTINUE,
+    REMATCH
+}
+
+public class EndScenePlayer : MonoBehaviour {
+
+    public int place;
+    private int playerNumber;
+    private EndScenePlayerState state;
+
+    public SpriteRenderer playerCoin;
+    public SpriteRenderer characterImage;
+
+    public EndSceneController endSceneController;
+
+    Controller controller;
+    // Use this for initialization
+    void Start()
+    {
+        playerNumber = EndData.instance.completionOrder[place - 1];
+        BarnoutPlayer p = PlayerData.instance.barnoutPlayers[playerNumber - 1];
+        if (p.IsActive())
+        {
+            state = EndScenePlayerState.SELECTING;
+            controller = Inputs.GetController(playerNumber);
+        }
+        else
+        {
+            state = EndScenePlayerState.INACTIVE;
+            gameObject.SetActive(false);
+        }
+
+        if (place == 1)
+        {
+            characterImage.sprite = endSceneController.GetWinnerSprite(p.GetCharacter());
+        }
+        else
+        {
+            characterImage.sprite = endSceneController.GetLoserCoin(p.GetCharacter());
+        }
+        playerCoin.sprite = endSceneController.GetPlayerCoin(playerNumber);
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (state == EndScenePlayerState.SELECTING)
+        {
+            if (controller.GetA())
+            {
+                state = EndScenePlayerState.CONTINUE;
+            }
+            if (controller.GetY())
+            {
+                state = EndScenePlayerState.REMATCH;
+            }
+        }
+	}
+
+    public EndScenePlayerState GetState()
+    {
+        return state;
+    }
+
+}
