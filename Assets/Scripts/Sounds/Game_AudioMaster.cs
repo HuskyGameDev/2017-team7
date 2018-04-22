1f842511d7af612da618e7414934b2ce0c743b40
tleichte <tleichte@mtu.edu>
2018-04-22 15:39:57 -0400

    Adjusted final beep

--- Assets/Scripts/Sounds/Game_AudioMaster.cs
+++ Assets/Scripts/Sounds/Game_AudioMaster.cs
@@ -12,17 +12,12 @@ public class Game_AudioMaster : AudioMaster {
     public void PlayRaceStart()
     {
         DoEvent("Play_Go");
-        StartCoroutine(DelayGameMusic());
+        DoEvent("Play_Game_Music");
     }
     public void EndRaceMusic()
     {
         //TODO: create end race music event
         //DoEvent("Play_Go")
         //DoEvent("Play_Whistle")
-    }
-    IEnumerator DelayGameMusic()
-    {
-        yield return new WaitForSecondsRealtime(0.2f);
-        DoEvent("Play_Game_Music");
     }
 }
